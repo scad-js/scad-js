db62e188d009fa7336d6779ddaaab724dd6fbcb6
Lior Haim <lior@dotcore.co.il>
2020-07-25 17:55:00 +0300

    Updating examples

--- examples/mobius_strip.js
+++ examples/mobius_strip.js
@@ -1,6 +1,9 @@
 const S = require('../src/index.js');
 
-const wall = p => S.rotate({ a: [0, 0, 2 * p]}, S.translate({v: [0, 40, 0]}, S.rotate({a: [p, 0, 0]}, S.cylinder({ h: 20, r: 1}))));
+const wall = p => S.cylinder({ h: 20, r: 1 })
+  .rotate({ a: [p, 0, 0]})
+  .translate({v: [0, 40, 0]})
+  .rotate({ a: [0, 0, 2 * p] });
 
 const walls = [];
 
@@ -8,6 +11,4 @@ for (let i = 0; i <= 360; i += 5) {
   walls.push(S.hull(wall(i), wall(i + 5)));
 }
 
-const output = S.parse(S.union(...walls));
-
-console.log(output);
+console.log(S.union(...walls).serialize());
--- examples/turners_cube.js
+++ examples/turners_cube.js
@@ -4,13 +4,14 @@ const cube1 = x => S.cube({size: x, center: true});
 const cylinder1 = x => S.cylinder({d: x, h: 12, center: true});
 
 const cy3 = x => S.union(
-  S.rotate({a: [90, 0, 0]}, cylinder1(x)),
-  S.rotate({a: [0, 90, 0]}, cylinder1(x)),
-  S.rotate({a: [0, 0, 90]}, cylinder1(x)),
+  cylinder1(x).rotate({a: [90, 0, 0]}),
+  cylinder1(x).rotate({a: [0, 90, 0]}),
+  cylinder1(x).rotate({a: [0, 0, 90]}),
 );
 
 const cutCube = x => S.difference(cube1(x), cy3(x - 0.5));
 
-const x = S.parse(S.union(...[4, 6, 8, 10].map(x => cutCube(x))));
+
+const x = S.union(...[4, 6, 8, 10].map(x =>cutCube(x))).serialize();
 
 console.log(x);
