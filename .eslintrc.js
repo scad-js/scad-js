b55d17e7c6cd5ea197d86a23dd17bff722f70811
20lives <lior@dotcore.co.il>
2020-10-18 03:50:06 +0300

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'node': true,
+    'es2021': true,
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 12,
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'always',
+    ],
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+  },
+};
