ecd90f4308d7dc8028c71348797deef6b804ebd7
20lives <lior@dotcore.co.il>
2020-10-27 16:22:52 +0200

    Add support for objects special parameters ($fn, $fa, $fs)

--- src/objects.js
+++ src/objects.js
@@ -5,6 +5,10 @@ const serialize = require('./serialize.js');
 const center = true;
 const undef = 'undef';
 
+const fn = 0;
+const fa = 12;
+const fs = 2;
+
 const object = type => params => ({
   type,
   params,
@@ -13,13 +17,48 @@ const object = type => params => ({
   serialize,
 });
 
-const circle = (r = 1) => object('circle')({ r });
-const square = (size = [1, 1]) => object('square')({ size, center });
-const polygon = (points = undef, paths = undef, convexity = 1) => object('polygon')({ points, paths, convexity });
+const circle = (r = 1, params = {}) => object('circle')({
+  r,
+  $fn: params?.$fn ?? fn,
+  $fa: params?.$fa ?? fa,
+  $fs: params?.$fs ?? fs,
+});
 
-const sphere = (r = 1) => object('sphere')({ r });
-const cube = (size = [1, 1, 1]) => object('cube')({ size, center});
-const cylinder = (h = 1, r = 1) => object('cylinder')({ h, ...(Array.isArray(r) ? { r1: r[0], r2: r[1] } : { r }), center});
-const polyhedron = (points = undef, faces = undef, convexity = 1) => object('polyhedron')({ points, faces, convexity });
+const square = (size = [1, 1]) => object('square')({
+  size,
+  center,
+});
+
+const polygon = (points = undef, paths = undef, convexity = 1) => object('polygon')({
+  points,
+  paths,
+  convexity,
+});
+
+const sphere = (r = 1, params = {}) => object('sphere')({
+  r,
+  $fn: params?.$fn ?? fn,
+  $fa: params?.$fa ?? fa,
+  $fs: params?.$fs ?? fs,
+});
+
+const cube = (size = [1, 1, 1]) => object('cube')({
+  size,
+  center,
+});
+
+const cylinder = (h = 1, r = 1, params = {}) => object('cylinder')({
+  h,
+  ...(Array.isArray(r) ? { r1: r[0], r2: r[1] } : { r }),
+  center,
+  $fn: params?.$fn ?? fn,
+  $fa: params?.$fa ?? fa,
+  $fs: params?.$fs ?? fs,
+});
+const polyhedron = (points = undef, faces = undef, convexity = 1) => object('polyhedron')({
+  points,
+  faces,
+  convexity,
+});
 
 module.exports = { circle, square, polygon, sphere, cube, cylinder, polyhedron };
