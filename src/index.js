4c37df86f016977058d026c3db7e822c126fe9c6
20lives <lior@dotcore.co.il>
2020-10-27 19:07:35 +0200

    Update serialize to support global special variables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const operations = require('./operations.js');
 const objects = require('./objects.js');
-const serialize = require('./serialize.js');
 
-module.exports = { ...objects, ...operations, serialize };
+module.exports = { ...objects, ...operations };
