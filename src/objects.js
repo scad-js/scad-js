f5027790944141a8e26dfa60926bc1c09817032c
20lives <lior@dotcore.co.il>
2020-10-31 03:59:16 +0200

    Change to OLOO pattern

--- src/objects.js
+++ src/objects.js
@@ -2,63 +2,55 @@ const transformations = require('./transformations.js');
 const modifiers = require('./modifiers.js');
 const serialize = require('./serialize.js');
 
-const center = true;
-const undef = 'undef';
-
 const fn = 0;
 const fa = 12;
 const fs = 2;
 
-const object = type => params => ({
-  type,
-  params,
-  ...transformations,
-  ...modifiers,
-  serialize,
-});
+const undef = 'undef';
 
-const circle = (r = 1, params = {}) => object('circle')({
-  r,
-  $fn: params?.$fn ?? fn,
-  $fa: params?.$fa ?? fa,
-  $fs: params?.$fs ?? fs,
-});
+const center = true;
 
-const square = (size = [1, 1]) => object('square')({
-  size,
-  center,
-});
+const object = type => params =>
+  Object.assign(Object.create({ ...transformations, ...modifiers, serialize }), { type, params });
 
-const polygon = (points = undef, paths = undef, convexity = 1) => object('polygon')({
-  points,
-  paths,
-  convexity,
-});
+module.exports = {
+  circle: (r = 1, params = {}) => object('circle')({
+    r,
+    $fn: params.$fn || fn,
+    $fa: params.$fa || fa,
+    $fs: params.$fs || fs,
 
-const sphere = (r = 1, params = {}) => object('sphere')({
-  r,
-  $fn: params?.$fn ?? fn,
-  $fa: params?.$fa ?? fa,
-  $fs: params?.$fs ?? fs,
-});
-
-const cube = (size = [1, 1, 1]) => object('cube')({
-  size,
-  center,
-});
-
-const cylinder = (h = 1, r = 1, params = {}) => object('cylinder')({
-  h,
-  ...(Array.isArray(r) ? { r1: r[0], r2: r[1] } : { r }),
-  center,
-  $fn: params?.$fn ?? fn,
-  $fa: params?.$fa ?? fa,
-  $fs: params?.$fs ?? fs,
-});
-const polyhedron = (points = undef, faces = undef, convexity = 1) => object('polyhedron')({
-  points,
-  faces,
-  convexity,
-});
-
-module.exports = { circle, square, polygon, sphere, cube, cylinder, polyhedron };
+  }),
+  square: (size = [1, 1]) => object('square')({
+    size,
+    center,
+  }),
+  polygon: (points = undef, paths = undef, convexity = 1) => object('polygon')({
+    points,
+    paths,
+    convexity,
+  }),
+  sphere: (r = 1, params = {}) => object('sphere')({
+    r,
+    $fn: params.$fn || fn,
+    $fa: params.$fa || fa,
+    $fs: params.$fs || fs,
+  }),
+  cube: (size = [1, 1, 1]) => object('cube')({
+    size,
+    center,
+  }),
+  cylinder: (h = 1, r = 1, params = {}) => object('cylinder')({
+    h,
+    ...(Array.isArray(r) ? { r1: r[0], r2: r[1] } : { r }),
+    center,
+    $fn: params.$fn || fn,
+    $fa: params.$fa || fa,
+    $fs: params.$fs || fs,
+  }),
+  polyhedron: (points = undef, paths = undef, convexity = 1) => object('polyhedron')({
+    points,
+    paths,
+    convexity,
+  }),
+};
--- src/transformations.js
+++ src/transformations.js
@@ -1,78 +1,56 @@
-const serialize = require('./serialize.js');
 const modifiers = require('./modifiers.js');
-
+const serialize = require('./serialize.js');
 const undef = 'undef';
 
-const transformation = type => function(params) {
-  return {
-    type,
-    params,
-    children: [ this ],
-    ...transformations,
-    ...modifiers,
-    serialize,
-  };
-};
-
-const translate = function(v) {
-  return transformation('translate').call(this, { v });
-};
-const scale = function(v) {
-  return transformation('scale').call(this, { v });
-};
-const resize = function(v, auto = false) {
-  return transformation('resize').call(this, { v, auto });
-};
-const mirror = function(v) {
-  return transformation('mirror').call(this, { v });
-};
-const color = function(c, alpha = 1) {
-  return transformation('color').call(this, { c, alpha });
-};
-const rotate = function(a, v = undef) {
-  return transformation('rotate').call(this, { a, v });
-};
-const radius_offset = function(r = undef) {
-  return transformation('offset').call(this, { r, delta: undef, chamfer: undef });
-};
-const delta_offset = function(delta, chamfer = false) {
-  return transformation('offset').call(this, { r: undef, delta, chamfer });
-};
-const projection = function(cut = false) {
-  return transformation('projection').call(this, { cut });
-};
-const linear_extrude = function(height = 0, params = {}) {
-  return transformation('linear_extrude').call(this, {
-    height,
-    center: params.center ?? false,
-    convexity: params.convexity ?? undef,
-    twist: params.twist ?? undef,
-    slices: params.slices ?? undef,
-    scale: params.scale ?? 1,
-    $fn: params?.$fn ?? 20 ,
-  });
-};
-
-const rotate_extrude = function(angle = 360, params = {}) {
-  return transformation('rotate_extrude').call(this, {
-    angle,
-    convexity: params?.convexity ?? 2,
-    $fn: params?.$fn ?? 10,
-  });
-};
+const transformation = type => (obj, params) =>
+  Object.assign(Object.create({ ...transformations, ...modifiers, serialize }), { type, params, children: [ obj ] });
 
 const transformations = {
-  translate,
-  rotate,
-  scale,
-  resize,
-  mirror,
-  color,
-  radius_offset,
-  delta_offset,
-  projection,
-  linear_extrude,
-  rotate_extrude,
+  translate (v) {
+    return transformation('translate')(this, { v });
+  },
+  scale (v) {
+    return transformation('scale')(this, { v });
+  },
+  resize (v, auto = false) {
+    return transformation('resize')(this, { v, auto });
+  },
+  mirror (v) {
+    return transformation('mirror')(this, { v });
+  },
+  color (c, alpha = 1) {
+    return transformation('color')(this, { c, alpha });
+  },
+  rotate (a, v = undef) {
+    return transformation('rotate')(this, { a, v });
+  },
+  radius_offset (r = undef) {
+    return transformation('offset')(this, { r, delta : undef, chamfer: undef });
+  },
+  delta_offset (delta, chamfer = false) {
+    return transformation('offset')(this, { r: undef, delta, chamfer });
+  },
+  projection (cut = false) {
+    return transformation('projection')(this, { cut });
+  },
+  linear_extrude (height = undef, params = {}) {
+    return transformation('linear_extrude')(this, {
+      height,
+      center: params.center || false,
+      convexity: params.convexity || undef,
+      twist: params.twist || undef,
+      slices: params.slices || undef,
+      scale: params.scale || 1,
+      $fn: params.$fn || 20,
+    });
+  },
+  rotate_extrude (angle = 360, params = {}) {
+    return transformation('rotate_extrude')(this, {
+      angle,
+      convexity: params.convexity || 2,
+      $fn: params.$fn || 10,
+    });
+  },
 };
 
 module.exports = transformations;
