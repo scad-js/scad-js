91f11ab51d2198e61c42ba221931edadf7e8bffc
twentylives <lior@dotcore.co.il>
2025-12-26 22:56:19 +0200

    apply biome formatting

--- src/index.ts
+++ src/index.ts
@@ -2,59 +2,55 @@
  * scad-js: Generate OpenSCAD solid models with TypeScript
  */
 
-// Re-export types
-export * from './types';
-
-// Re-export utilities
-export { create, undef } from './utils';
-
-// Re-export operations
-export {
-  union,
-  difference,
-  intersection,
-  hull,
-  minkowski,
-  chain_hull,
-} from './operations';
-
 // Re-export objects
 export {
   circle,
-  square,
-  polygon,
   cube,
   cylinder,
+  polygon,
   polyhedron,
-  sphere,
   rounded_cube,
   rounded_square,
+  sphere,
+  square,
 } from './objects';
-
+// Re-export operations
+export {
+  chain_hull,
+  difference,
+  hull,
+  intersection,
+  minkowski,
+  union,
+} from './operations';
 // Re-export transformations
 export {
-  translate,
-  translate_x,
-  translate_y,
-  translate_z,
-  scale,
-  scale_x,
-  scale_y,
-  scale_z,
-  resize,
+  color,
+  delta_offset,
+  linear_extrude,
   mirror,
   mirror_x,
   mirror_y,
   mirror_z,
+  multmatrix,
+  projection,
+  radius_offset,
+  resize,
   rotate,
+  rotate_extrude,
   rotate_x,
   rotate_y,
   rotate_z,
-  color,
-  multmatrix,
-  radius_offset,
-  delta_offset,
-  projection,
-  linear_extrude,
-  rotate_extrude,
+  scale,
+  scale_x,
+  scale_y,
+  scale_z,
+  translate,
+  translate_x,
+  translate_y,
+  translate_z,
 } from './transformations';
+// Re-export types
+export * from './types';
+// Re-export utilities
+export { create, undef } from './utils';
