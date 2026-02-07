scad-js/scad-js

commit efc4588edc46fea2e33d3e821bc6570adab0b131

Refactor with Typescript/Bun/Vitest/Biome!

src/serialize.ts
@@ -1,0 +1,41 @@
+import type { ScadObject } from './types';
+
+/**
+ * Creates indentation string with specified level
+ */
+const indent = (n: number): string => ' '.repeat(2 * n);
+
+/**
+ * Converts a value to its OpenSCAD string representation
+ */
+const stringify = (x: any): string =>
+  typeof x === 'string' ? (x === 'undef' || x.includes('$t') ? x : `"${x}"`) : JSON.stringify(x).replace(/"/g, '').split(',').join(', ');
+
+/**
+ * Converts object parameters to OpenSCAD parameter string
+ */
+const paramsStr = (params: Record<string, any>): string =>
+  Object.entries(params)
+    .map((x) => `${x[0]} = ${stringify(x[1])}`)
+    .join(', ');
+
+/**
+ * Serializes a ScadObject to OpenSCAD code
+ */
+function serialize(this: ScadObject, vars: Record<string, any> = {}, depth = 0): string {
+  const { type, params = {}, children } = this;
+  let output = Object.entries(vars)
+    .map(([a, b]) => `${a} = ${b};\n`)
+    .join('');
+  output += `${indent(depth)}${type}(${paramsStr(params)})`;
+
+  if (children && children.length > 0) {
+    output += `\n${indent(depth)}{\n${children.map((child) => serialize.call(child, {}, depth + 1)).join('')}${indent(depth)}}\n`;
+  } else {
+    output += ';\n';
+  }
+
+  return output;
+}
+
+export default serialize;

test/serialize.test.ts
@@ -1,0 +1,73 @@
+import { describe, it, expect } from 'vitest';
+import * as S from '../src';
+
+describe('Serialize', () => {
+  it('should serialize an object with number value', () => {
+    expect(S.square().serialize()).toBe(
+      'square(size = [1, 1], center = true);\n'
+    );
+  });
+
+  it('should serialize an object with animation value', () => {
+    // Cast to any to allow string values for testing
+    expect((S.square as any)('$t').serialize()).toBe(
+      'square(size = $t, center = true);\n'
+    );
+  });
+
+  it('should serialize an object with array animation value', () => {
+    // Cast to any to allow string values for testing
+    expect((S.cube as any)(['$t', '2 * $t', 5]).serialize()).toBe(
+      'cube(size = [$t, 2 * $t, 5], center = true);\n'
+    );
+  });
+
+  it('should serialize an object with color transformation', () => {
+    expect(S.cube(8).color('green', 0.6).serialize()).toBe(
+      'color(c = "green", alpha = 0.6)\n' +
+      '{\n' +
+      '  cube(size = 8, center = true);\n' +
+      '}\n'
+    );
+  });
+
+  it('should serialize an object with radius offset', () => {
+    // Access radius_offset through the index signature to avoid type error
+    const square = S.square();
+    const result = (square as any).radius_offset(5).serialize();
+    expect(result).toBe(
+      'offset(r = 5, delta = undef, chamfer = undef)\n' +
+      '{\n' +
+      '  square(size = [1, 1], center = true);\n' +
+      '}\n'
+    );
+  });
+
+  it('should serialize a square with numeric size', () => {
+    expect(S.square(5).serialize()).toBe(
+      'square(size = 5, center = true);\n'
+    );
+  });
+
+  it('should serialize an object with debug modifier', () => {
+    expect(S.square(8).debug().serialize()).toBe(
+      '#union()\n{\n  square(size = 8, center = true);\n}\n'
+    );
+  });
+
+  it('should serialize a transformed object', () => {
+    expect(S.square([4, 5]).translate([5, 6, 7]).serialize()).toBe(
+      'translate(v = [5, 6, 7])\n{\n  square(size = [4, 5], center = true);\n}\n'
+    );
+  });
+
+  it('should serialize an operation', () => {
+    expect(S.union(S.sphere(), S.cube()).serialize()).toBe(
+      'union()\n' +
+      '{\n' +
+      '  sphere(r = 1);\n' +
+      '  cube(size = [1, 1, 1], center = true);\n' +
+      '}\n'
+    );
+  });
+}); 

.eslintrc.js
@@ -1,35 +1,0 @@
-module.exports = {
-  'env': {
-    'mocha': true,
-    'browser': true,
-    'commonjs': true,
-    'node': true,
-    'es2021': true,
-  },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'ecmaVersion': 12,
-  },
-  'rules': {
-    'indent': [
-      'error',
-      2,
-    ],
-    'linebreak-style': [
-      'error',
-      'unix',
-    ],
-    'quotes': [
-      'error',
-      'single',
-    ],
-    'semi': [
-      'error',
-      'always',
-    ],
-    'comma-dangle': [
-      'error',
-      'always-multiline',
-    ],
-  },
-};

.travis.yml
@@ -1,10 +1,0 @@
-language: node_js
-node_js:
-  - '12'
-  - '14'
-  - '15'
-script:
-  - yarn
-  - yarn test
-  - yarn coverage
-  - yarn lint

src/index.js
@@ -1,4 +1,0 @@
-const operations = require('./operations.js');
-const objects = require('./objects.js');
-
-module.exports = { ...objects, ...operations };

src/index.ts
@@ -1,0 +1,60 @@
+/**
+ * scad-js: Generate OpenSCAD solid models with TypeScript
+ */
+
+// Re-export types
+export * from './types';
+
+// Re-export utilities
+export { create, undef } from './utils';
+
+// Re-export operations
+export {
+  union,
+  difference,
+  intersection,
+  hull,
+  minkowski,
+  chain_hull,
+} from './operations';
+
+// Re-export objects
+export {
+  circle,
+  square,
+  polygon,
+  cube,
+  cylinder,
+  polyhedron,
+  sphere,
+  rounded_cube,
+  rounded_square,
+} from './objects';
+
+// Re-export transformations
+export {
+  translate,
+  translate_x,
+  translate_y,
+  translate_z,
+  scale,
+  scale_x,
+  scale_y,
+  scale_z,
+  resize,
+  mirror,
+  mirror_x,
+  mirror_y,
+  mirror_z,
+  rotate,
+  rotate_x,
+  rotate_y,
+  rotate_z,
+  color,
+  multmatrix,
+  radius_offset,
+  delta_offset,
+  projection,
+  linear_extrude,
+  rotate_extrude,
+} from './transformations';

src/modifiers.js
@@ -1,26 +1,0 @@
-const transformations = require('./transformations.js');
-const serialize = require('./serialize.js');
-const { create } = require('./utils.js');
-
-const modifier = (type, ...children) => create({ ...transformations, ...modifiers, serialize }, {
-  type: type + 'union',
-  children,
-});
-
-const modifiers = {
-  disable () {
-    return modifier('*', this);
-  },
-  show_only() {
-    return modifier('!', this);
-  },
-  debug() {
-    return modifier('#', this);
-  },
-  background() {
-    return modifier('%', this);
-  },
-};
-
-
-module.exports = modifiers;

src/modifiers.ts
@@ -1,0 +1,53 @@
+import serialize from './serialize';
+import * as transformations from './transformations';
+import type { ScadObject, ScadType } from './types';
+import { create } from './utils';
+
+/**
+ * Creates a modified version of a ScadObject with a specific modifier prefix
+ */
+const modifier = (typePrefix: string, ...children: ScadObject[]): ScadObject => {
+  const type = `${typePrefix}union` as ScadType;
+  return create(
+    { ...transformations, ...modifiers, serialize },
+    {
+      type,
+      children,
+    },
+  );
+};
+
+/**
+ * Modifiers that can be applied to ScadObjects
+ */
+const modifiers = {
+  /**
+   * Disable the object (equivalent to * in OpenSCAD)
+   */
+  disable(this: ScadObject): ScadObject {
+    return modifier('*', this);
+  },
+
+  /**
+   * Show only this object (equivalent to ! in OpenSCAD)
+   */
+  root(this: ScadObject): ScadObject {
+    return modifier('!', this);
+  },
+
+  /**
+   * Show object in debug mode (equivalent to # in OpenSCAD)
+   */
+  debug(this: ScadObject): ScadObject {
+    return modifier('#', this);
+  },
+
+  /**
+   * Set as background (equivalent to % in OpenSCAD)
+   */
+  background(this: ScadObject): ScadObject {
+    return modifier('%', this);
+  },
+};
+
+export default modifiers;

commit 2c89c548f86769de7e450ad617d00591adbe2bfe

fix: make stringify recursive to handle nested arrays
Nested arrays (used by polygon, polyhedron, multmatrix) were being
flattened during serialization, producing invalid OpenSCAD output.

This was a regression introduced in efc4588 (TypeScript refactor) which
replaced the original JSON.stringify-based approach with a flat
Array.join(). The fix makes stringify recursive so inner arrays are
properly wrapped in brackets.

Closes #39

src/serialize.ts
@@ -7,7 +7,7 @@ const stringify = (x: string | string[]): string => {
     return x === 'undef' || x.includes('$t') ? x : `"${x}"`;
   }
   if (Array.isArray(x)) {
-    return `[${x.join(', ')}]`;
+    return `[${x.map((i) => stringify(i)).join(', ')}]`;
   }
   return String(x);
 };

test/serialize.test.ts
@@ -1,83 +1,145 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import * as S from '../src';
 
 describe('Serialize', () => {
   it('should serialize an object with number value', () => {
-    expect(S.square().serialize()).toBe(
-      'square(size = [1, 1], center = true);\n'
-    );
+    expect(S.square().serialize()).toBe('square(size = [1, 1], center = true);\n');
   });
 
   it('should serialize an object with animation value', () => {
     // Cast to any to allow string values for testing
-    expect((S.square as any)('$t').serialize()).toBe(
-      'square(size = $t, center = true);\n'
-    );
+    expect((S.square as any)('$t').serialize()).toBe('square(size = $t, center = true);\n');
   });
 
   it('should serialize an object with array animation value', () => {
     // Cast to any to allow string values for testing
-    expect((S.cube as any)(['$t', '2 * $t', 5]).serialize()).toBe(
-      'cube(size = [$t, 2 * $t, 5], center = true);\n'
-    );
+    expect((S.cube as any)(['$t', '2 * $t', 5]).serialize()).toBe('cube(size = [$t, 2 * $t, 5], center = true);\n');
   });
 
   it('should serialize an object with color transformation', () => {
-    expect(S.cube(8).color('green', 0.6).serialize()).toBe(
-      'color(c = "green", alpha = 0.6)\n' +
-      '{\n' +
-      '  cube(size = 8, center = true);\n' +
-      '}\n'
-    );
+    expect(S.cube(8).color('green', 0.6).serialize()).toBe('color(c = "green", alpha = 0.6)\n' + '{\n' + '  cube(size = 8, center = true);\n' + '}\n');
   });
 
   it('should serialize an object with radius offset', () => {
     // Access radius_offset through the index signature to avoid type error
     const square = S.square();
     const result = (square as any).radius_offset(5).serialize();
-    expect(result).toBe(
-      'offset(r = 5, delta = undef, chamfer = undef)\n' +
-      '{\n' +
-      '  square(size = [1, 1], center = true);\n' +
-      '}\n'
-    );
+    expect(result).toBe('offset(r = 5, delta = undef, chamfer = undef)\n' + '{\n' + '  square(size = [1, 1], center = true);\n' + '}\n');
   });
 
   it('should serialize a square with numeric size', () => {
-    expect(S.square(5).serialize()).toBe(
-      'square(size = 5, center = true);\n'
-    );
+    expect(S.square(5).serialize()).toBe('square(size = 5, center = true);\n');
   });
 
   it('should serialize an object with debug modifier', () => {
-    expect(S.square(8).debug().serialize()).toBe(
-      '#union()\n{\n  square(size = 8, center = true);\n}\n'
-    );
+    expect(S.square(8).debug().serialize()).toBe('#union()\n{\n  square(size = 8, center = true);\n}\n');
   });
 
   it('should serialize a transformed object', () => {
-    expect(S.square([4, 5]).translate([5, 6, 7]).serialize()).toBe(
-      'translate(v = [5, 6, 7])\n{\n  square(size = [4, 5], center = true);\n}\n'
-    );
+    expect(S.square([4, 5]).translate([5, 6, 7]).serialize()).toBe('translate(v = [5, 6, 7])\n{\n  square(size = [4, 5], center = true);\n}\n');
   });
 
   it('should serialize an operation', () => {
-    expect(S.union(S.sphere(), S.cube()).serialize()).toBe(
-      'union()\n' +
-      '{\n' +
-      '  sphere(r = 1);\n' +
-      '  cube(size = [1, 1, 1], center = true);\n' +
-      '}\n'
-    );
+    expect(S.union(S.sphere(), S.cube()).serialize()).toBe('union()\n' + '{\n' + '  sphere(r = 1);\n' + '  cube(size = [1, 1, 1], center = true);\n' + '}\n');
   });
 
   it('should handle serialize arguments', () => {
-    expect(S.square(5).serialize({ $fn: 10, $fa: 10, $fs: 10 })).toBe(
-      '$fn = 10;\n' +
-      '$fa = 10;\n' +
-      '$fs = 10;\n' +
-      'square(size = 5, center = true);\n'
-    );
+    expect(S.square(5).serialize({ $fn: 10, $fa: 10, $fs: 10 })).toBe('$fn = 10;\n' + '$fa = 10;\n' + '$fs = 10;\n' + 'square(size = 5, center = true);\n');
+  });
+
+  describe('nested array serialization', () => {
+    it('should serialize polygon points as nested arrays', () => {
+      const model = S.polygon([
+        [0, 50],
+        [50, -50],
+        [-50, -50],
+      ]);
+      expect(model.serialize()).toBe('polygon(points = [[0, 50], [50, -50], [-50, -50]], paths = undef, convexity = 1);\n');
+    });
+
+    it('should serialize polygon with explicit paths as nested arrays', () => {
+      const model = S.polygon(
+        [
+          [0, 0],
+          [10, 0],
+          [10, 10],
+          [0, 10],
+        ],
+        [[0, 1, 2, 3]],
+      );
+      expect(model.serialize()).toBe('polygon(points = [[0, 0], [10, 0], [10, 10], [0, 10]], paths = [[0, 1, 2, 3]], convexity = 1);\n');
+    });
+
+    it('should serialize polygon with multiple paths', () => {
+      const model = S.polygon(
+        [
+          [0, 0],
+          [10, 0],
+          [10, 10],
+          [0, 10],
+          [3, 3],
+          [7, 3],
+          [7, 7],
+          [3, 7],
+        ],
+        [
+          [0, 1, 2, 3],
+          [4, 5, 6, 7],
+        ],
+      );
+      expect(model.serialize()).toBe(
+        'polygon(points = [[0, 0], [10, 0], [10, 10], [0, 10], [3, 3], [7, 3], [7, 7], [3, 7]], paths = [[0, 1, 2, 3], [4, 5, 6, 7]], convexity = 1);\n',
+      );
+    });
+
+    it('should serialize polyhedron points and faces as nested arrays', () => {
+      const model = S.polyhedron(
+        [
+          [0, 0, 0],
+          [10, 0, 0],
+          [5, 10, 0],
+          [5, 5, 10],
+        ],
+        [
+          [0, 1, 2],
+          [0, 1, 3],
+          [1, 2, 3],
+          [0, 2, 3],
+        ],
+      );
+      expect(model.serialize()).toBe(
+        'polyhedron(points = [[0, 0, 0], [10, 0, 0], [5, 10, 0], [5, 5, 10]], paths = [[0, 1, 2], [0, 1, 3], [1, 2, 3], [0, 2, 3]], convexity = 1);\n',
+      );
+    });
+
+    it('should serialize multmatrix with nested matrix array', () => {
+      const matrix = [
+        [1, 0, 0, 10],
+        [0, 1, 0, 20],
+        [0, 0, 1, 30],
+        [0, 0, 0, 1],
+      ];
+      const model = S.cube(5).multmatrix(matrix);
+      expect(model.serialize()).toBe('multmatrix(m = [[1, 0, 0, 10], [0, 1, 0, 20], [0, 0, 1, 30], [0, 0, 0, 1]])\n' + '{\n' + '  cube(size = 5, center = true);\n' + '}\n');
+    });
+
+    it('should serialize polygon with negative and decimal coordinates', () => {
+      const model = S.polygon([
+        [-1.5, 2.7],
+        [3.14, -0.5],
+        [0, 100],
+      ]);
+      expect(model.serialize()).toBe('polygon(points = [[-1.5, 2.7], [3.14, -0.5], [0, 100]], paths = undef, convexity = 1);\n');
+    });
+
+    it('should serialize polygon with single point', () => {
+      const model = S.polygon([[5, 10]]);
+      expect(model.serialize()).toBe('polygon(points = [[5, 10]], paths = undef, convexity = 1);\n');
+    });
+
+    it('should preserve flat arrays in non-nested parameters', () => {
+      expect(S.cube([2, 3, 4]).serialize()).toBe('cube(size = [2, 3, 4], center = true);\n');
+    });
   });
 
   it('should handle all OpenSCAD special variables', () => {
@@ -91,21 +153,21 @@ describe('Serialize', () => {
       $vpd: 140,
       $vpf: 22.5,
       $children: 3,
-      $preview: true
+      $preview: true,
     };
 
     expect(S.cube(10).serialize(vars)).toBe(
       '$fa = 12;\n' +
-      '$fs = 2;\n' +
-      '$fn = 50;\n' +
-      '$t = 0.5;\n' +
-      '$vpr = [30, 45, 60];\n' +
-      '$vpt = [10, 20, 30];\n' +
-      '$vpd = 140;\n' +
-      '$vpf = 22.5;\n' +
-      '$children = 3;\n' +
-      '$preview = true;\n' +
-      'cube(size = 10, center = true);\n'
+        '$fs = 2;\n' +
+        '$fn = 50;\n' +
+        '$t = 0.5;\n' +
+        '$vpr = [30, 45, 60];\n' +
+        '$vpt = [10, 20, 30];\n' +
+        '$vpd = 140;\n' +
+        '$vpf = 22.5;\n' +
+        '$children = 3;\n' +
+        '$preview = true;\n' +
+        'cube(size = 10, center = true);\n',
     );
   });
 });